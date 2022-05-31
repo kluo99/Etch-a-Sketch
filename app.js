a625ff6251d77ed6bddec254e1cb9fb80dbd9071
Kenneth <luo.kenneth1999@gmail.com>
2022-05-31 15:07:48 -0400

    Added a button that allows users to create a new grid

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const gridContainer = document.querySelector('#container');
+let gridContainer = document.querySelector('#container');
 
+//changes grid when you input a number
+function changeGrid() {
+    let answer = prompt("How many boxes do you want on each side for the new grid?");
+    if (answer > 100) {
+        alert("Limit is 100");
+    }
+    else if (isNaN(answer)) {
+        alert("You must enter a number");
+    }
+    else {
+        deleteGrid();
+        createNewGrid(answer);
+    }
+}
 
 //creates a column of 16 squares
 function createColumn(){
@@ -10,9 +24,12 @@ function createColumn(){
             let box = document.createElement('div');
             document.querySelectorAll('box');
             box.className = "box";
+
+            //mouse over effect
             box.addEventListener("mouseover", function() {
-                box.style.background = 'red';
+                box.style.background = 'blue';
             })
+
             column.appendChild(box);
         }
         gridContainer.appendChild(column);
@@ -24,6 +41,41 @@ function createGrid(){
     for (let i = 0; i < 16; i++) {
         createColumn();
     }
+}
+
+
+function createNewColumn(answer) {
+    for (let i = 0; i < answer; i++) {
+        let column = document.createElement('div');
+        column.className = "column";
+        for (let j = 0; i < answer; i++){
+            let box = document.createElement('div');
+            document.querySelectorAll('box');
+            box.className = "box";
+
+            //mouse over effect
+            box.addEventListener("mouseover", function() {
+                box.style.background = 'blue';
+            })
+
+            column.appendChild(box);
+        }
+        gridContainer.appendChild(column);
+    }
+}
+
+//creates a new grid depending on input from user
+function createNewGrid(answer){
+    for (let i = 0; i < answer; i++) {
+        createNewColumn(answer);
+    }
+}
+
+function deleteGrid() {
+    let destroy = document.getElementById('container');
+    while (destroy.hasChildNodes()) {
+        destroy.removeChild(destroy.firstChild);
+      }
 }
 
 createGrid();
